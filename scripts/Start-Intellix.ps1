608bbcd537e97a3ff4b981ab9e25cf71e92e8148
Christoph Weidling <Christoph.Weidling@docuware.com>
2020-09-29 17:21:03 +0200

    Removed mounting protection key path

--- scripts/Start-Intellix.ps1
+++ scripts/Start-Intellix.ps1
@@ -8,8 +8,4 @@ if (-not (Test-Path $env:E_SolRDataPath)) {
     mkdir -force $env:E_SolRDataPath | Out-Null
 }
 
-if (-not (Test-Path $env:E_DataProtectionKeysPath)) {
-    mkdir -force $env:E_DataProtectionKeysPath | Out-Null
-}
-
 docker-compose -f docker-compose.yml up -d -t 60 --force-recreate 
