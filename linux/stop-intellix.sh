9d460680793ef82fc0cd5ba5b5d15d4246c97349
Christoph Weidling <christoph.weidling@outlook.com>
2021-07-05 15:41:05 +0200

    Readd missing files (#15)
    * Add missing files
    
    * Readded database setup files
    
    * Fixed yml files
    
    Co-authored-by: Christoph Weidling <christoph.weidling@docuware.com>

---
+++ linux/stop-intellix.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+scriptRoot=$(readlink -f "$0")
+scriptRoot=$(dirname "$scriptRoot")
+
+echo "Stopping Intelligent Indexing..."
+docker-compose -f "$scriptRoot/setup/run/docker-compose.yml" down --remove-orphans
+echo "Intelligent Indexing is stopped."
---
+++ linux/update-intellix.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+withRestart=
+while [ $# -gt 0 ]; do
+  case "$1" in
+  --with-restart)
+    withRestart=1
+    ;;
+  *) ;;
+  esac
+  shift
+done
+
+scriptRoot=$(readlink -f "$0")
+scriptRoot=$(dirname "$scriptRoot")
+
+docker-compose -f "$scriptRoot/setup/run/docker-compose.yml" pull
+
+if [ -n "$withRestart" ] ; then
+    "$scriptRoot/stop-intellix.sh"
+    "$scriptRoot/start-intellix.sh"
+fi
