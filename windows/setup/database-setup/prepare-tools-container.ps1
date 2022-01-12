2fc12e0854704b73a58c00b4f2d62c234fa6b85c
Christoph Weidling <christoph.weidling@outlook.com>
2022-01-12 16:41:34 +0100

    Check if the package provider already exists (#20)

--- windows/setup/database-setup/prepare-tools-container.ps1
+++ windows/setup/database-setup/prepare-tools-container.ps1
@@ -1,5 +1,12 @@
 $progressPreference = 'silentlyContinue'
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+
+$existingNugetProvider = (Get-PackageProvider -Name NuGet) | Where-Object { ([System.Version]$_.Version) -gt ([System.Version]"1.8.5.201") } | Measure-Object
+if ($existingNugetProvider.Count -eq 0) {
+    Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force    
+} else {
+    Write-Host "PackageProvider NuGet is already installed."
+}
+
 Install-Module -Name SqlServer -Force -Scope CurrentUser
 
 Invoke-WebRequest "https://go.microsoft.com/fwlink/?linkid=2143496" -OutFile sqlpackage.zip
