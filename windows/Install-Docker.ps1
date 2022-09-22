ed4816ffed3a1d94dc1e92e7172cb2e6c585ddb5
Christoph Weidling <christoph.weidling@docuware.com>
2022-09-22 06:21:04 -0700

    Started with migrating to Mirantis engine

--- windows/Install-Docker.ps1
+++ windows/Install-Docker.ps1
@@ -1,15 +1,8 @@
-# Install NuGet Provider
-$existingNugetProvider = (Get-PackageProvider -Name NuGet -Force) | Where-Object { ([System.Version]$_.Version) -gt ([System.Version]"1.8.5.201") } | Measure-Object
-if ($existingNugetProvider.Count -eq 0) {
-    Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force    
-} else {
-    Write-Host "PackageProvider NuGet is already installed."
-}
+# Install Mirantis runtime
+$script = Invoke-WebRequest https://get.mirantis.com/install.ps1
+Invoke-Expression $($script.Content)
 
-# Install Docker
-Install-Module DockerMsftProvider -Force
-Install-Package Docker -ProviderName DockerMsftProvider -Force
 
 # Install Docker-Compose
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-Windows-x86_64.exe" -UseBasicParsing -OutFile $Env:ProgramFiles\Docker\docker-compose.exe
+Invoke-WebRequest "https://github.com/docker/compose/releases/download/v2.11.0/docker-compose-Windows-x86_64.exe" -UseBasicParsing -OutFile $Env:ProgramFiles\Docker\docker-compose.exe
