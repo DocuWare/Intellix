e97b910d7045ed84c87bfb1833f73227e0930610
Christoph Weidling <christoph.weidling@outlook.com>
2022-09-27 13:17:45 +0200

    Fixed spaces

--- windows/Start-Intellix.ps1
+++ windows/Start-Intellix.ps1
@@ -1,8 +1,10 @@
-﻿Write-Output "Getting container images. This can take up to a few minutes. Be patient..."
+Write-Output "Getting container images. This can take up to a few minutes. Be patient..."
 docker-compose -f $PSScriptRoot/setup/run/docker-compose.yml pull -q
-﻿Write-Output "Starting Intelligent Indexing..."
+
+Write-Output "Starting Intelligent Indexing..."
 docker-compose -f $PSScriptRoot/setup/run/docker-compose.yml up -d -t 60 --force-recreate
 Write-Output "Intelligent Indexing is started."
+
 $hn = $(hostname)
 Write-Output "Intelligent Indexing can be reached at http://localhost:8080/intellix-v2/ or http://$hn/intellix-v2/"
 Write-Output "To open the UI, browse to http://localhost:8080/intellix-v2/Html or http://$hn/intellix-v2/Html"
