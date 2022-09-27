82a410b962a08f3d9694c5d08ac27657b311f2c2
Christoph Weidling <christoph.weidling@outlook.com>
2022-09-27 13:08:00 +0200

    Use latest docker-compose

--- windows/Install-Docker.ps1
+++ windows/Install-Docker.ps1
@@ -7,4 +7,4 @@ Invoke-Expression $($script.Content)
 
 # Install Docker-Compose
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-Windows-x86_64.exe" -UseBasicParsing -OutFile $Env:ProgramFiles\Docker\docker-compose.exe
+Invoke-WebRequest "https://github.com/docker/compose/releases/download/v2.10.1/docker-compose-windows-x86_64.exe" -UseBasicParsing -OutFile $Env:ProgramFiles\Docker\docker-compose.exe
